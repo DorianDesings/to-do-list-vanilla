5d646672512e7225120de5f3f540a7b4901ae1e9
Dorian <dorianoriginaldesings@gmail.com>
2023-07-12 10:48:08 +0200

    toDo updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,58 @@
-/*
- <div class="task">
-        <input type="checkbox" />
-        <p>Tarea de ejemplo</p>
-        <button>X</button>
-      </div>
-*/
-
 const tasksElement = document.getElementById('tasks');
+const filtersElement = document.getElementById('filters');
 const formElement = document.getElementById('form');
 
-const deleteTask = event => {
-  event.target.parentElement.remove();
+let allTasks = [];
+
+const deleteTask = id => {
+  const updatedTasks = allTasks.filter(task => task.id !== id);
+  allTasks = updatedTasks;
+  printTasks(allTasks);
 };
 
-const createTask = task => {
+const saveTask = task => {
+  const newTask = {
+    id: Date.now(),
+    task,
+    completed: false
+  };
+  allTasks.push(newTask);
+  printTasks(allTasks);
+};
+
+const generateHtmlTask = task => {
   const newDiv = document.createElement('div');
   newDiv.classList.add('task');
 
   const newInput = document.createElement('input');
   newInput.type = 'checkbox';
+  newInput.checked = task.completed;
 
   const newText = document.createElement('p');
-  newText.textContent = task;
+  newText.textContent = task.task;
 
   const newButton = document.createElement('button');
   newButton.textContent = 'X';
 
-  newButton.addEventListener('click', deleteTask);
+  newButton.addEventListener('click', () => deleteTask(task.id));
 
   newDiv.append(newInput, newText, newButton);
 
-  tasksElement.append(newDiv);
+  return newDiv;
+};
+
+const printTasks = tasks => {
+  tasksElement.innerHTML = '';
+  const fragment = document.createDocumentFragment();
+  tasks.forEach(task => {
+    const newTask = generateHtmlTask(task);
+    fragment.append(newTask);
+  });
+  tasksElement.append(fragment);
 };
 
 formElement.addEventListener('submit', event => {
   event.preventDefault();
-  createTask(event.target.task.value);
+  saveTask(event.target.task.value);
   formElement.reset();
 });

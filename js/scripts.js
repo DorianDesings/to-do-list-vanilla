77dbb57d57ec4b653ce3151f965e4b11d6f78f23
Dorian <charo.desarrolloweb@gmail.com>
2023-07-13 19:26:58 +0200

    to-do-list-example

--- js/scripts.js
+++ js/scripts.js
@@ -2,98 +2,99 @@ const tasksElement = document.getElementById('tasks');
 const filtersElement = document.getElementById('filters');
 const formElement = document.getElementById('form');
 
+const filters = {
+  all: 0,
+  uncomplete: 1,
+  complete: 2
+};
+
 let allTasks = [];
 
-const deleteTask = id => {
-  const updatedTasks = allTasks.filter(task => task.id !== id);
-  allTasks = updatedTasks;
-  printTasks(allTasks);
+// Escuchar el evento de envío del formulario ✅
+// Obtener el valor de lo que hay escrito ✅
+// Meter tarea en el objeto de tareas ✅
+// Generar el HTML de cada tarea ✅
+// Meter el HTML en el contenedor de tareas ✅
+// Borrar tarea ✅
+// Actualizar tarea ✅
 
-  console.log('DELETE', allTasks);
+const generateHtmlForTask = task => {
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('task');
+  const newCheckbox = document.createElement('input');
+  newCheckbox.type = 'checkbox';
+  newCheckbox.checked = task.completed;
+  const newTask = document.createElement('p');
+  newTask.textContent = task.task;
+  const newButton = document.createElement('button');
+  newButton.textContent = 'X';
+
+  newButton.addEventListener('click', () => deleteTask(task.id));
+  newCheckbox.addEventListener('change', () => completeTask(task.id));
+
+  newDiv.append(newCheckbox, newTask, newButton);
+
+  return newDiv;
+};
+
+const insertTasks = () => {
+  tasksElement.textContent = '';
+  const fragment = document.createDocumentFragment();
+  allTasks.forEach(task => {
+    const newTask = generateHtmlForTask(task);
+    fragment.append(newTask);
+  });
+
+  tasksElement.append(fragment);
 };
 
 const saveTask = task => {
-  const newTask = {
-    id: Date.now(),
-    task,
-    completed: false
-  };
-  allTasks.push(newTask);
-  printTasks(allTasks);
-
-  console.log('CREATE', allTasks);
+  allTasks.push(task);
+  insertTasks();
 };
 
-const updateTask = id => {
-  const updatedTasks = allTasks.map(task => {
+const deleteTask = id => {
+  allTasks = allTasks.filter(task => task.id !== id);
+
+  insertTasks();
+  // All tasks menos la que coincida con el id
+};
+
+const completeTask = id => {
+  allTasks = allTasks.map(task => {
     if (task.id === id) {
       task.completed = !task.completed;
     }
     return task;
   });
-  allTasks = updatedTasks;
-  printTasks(allTasks);
 
-  console.log('UPDATE', allTasks);
+  console.log(allTasks);
+
+  insertTasks();
 };
 
-const generateHtmlTask = task => {
-  const newDiv = document.createElement('div');
-  newDiv.classList.add('task');
+const createTask = task => {
+  const newTask = {
+    id: Date.now(),
+    task: task,
+    completed: false
+  };
 
-  const newInput = document.createElement('input');
-  newInput.type = 'checkbox';
-  newInput.checked = task.completed;
-
-  newInput.addEventListener('change', () => updateTask(task.id));
-
-  const newText = document.createElement('p');
-  newText.textContent = task.task;
-
-  const newButton = document.createElement('button');
-  newButton.textContent = 'X';
-
-  newButton.addEventListener('click', () => deleteTask(task.id));
-
-  newDiv.append(newInput, newText, newButton);
-
-  return newDiv;
-};
-
-const printTasks = tasks => {
-  tasksElement.innerHTML = '';
-  const fragment = document.createDocumentFragment();
-  tasks.forEach(task => {
-    const newTask = generateHtmlTask(task);
-    fragment.append(newTask);
-  });
-  tasksElement.append(fragment);
+  saveTask(newTask);
 };
 
 const filterTasks = filter => {
-  const parsedFilter = Number(filter);
-  let tasksToRender = [...allTasks];
-  if (parsedFilter === 1) {
-    tasksToRender = allTasks.filter(task => !task.completed);
-    console.log('UNCOMPLETED', tasksToRender);
-  } else if (parsedFilter === 2) {
-    tasksToRender = allTasks.filter(task => task.completed);
-    console.log('COMPLETED', tasksToRender);
-  } else {
-    console.log('ALL', tasksToRender);
-  }
-  printTasks(tasksToRender);
+  console.log(filter);
 };
 
 formElement.addEventListener('submit', event => {
   event.preventDefault();
-  const task = event.target.task.value;
-  if (!task) return;
-  saveTask(task);
-  formElement.reset();
+  if (event.target.task.value === '') return;
+  createTask(event.target.task.value);
+  event.target.reset();
 });
 
-filtersElement.addEventListener('click', e => {
-  if (e.target.tagName !== 'BUTTON') return;
-  filterTasks(e.target.dataset.filter);
+filtersElement.addEventListener('click', event => {
+  if (event.target.tagName !== 'BUTTON') return;
+  filterTasks(event.target.dataset.filter);
 });
